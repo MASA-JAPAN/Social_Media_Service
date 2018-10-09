0c7e052776696cdfd3a867210ba5830ecb77ff50
MASA-JAPAN <masaifactoryline@gmail.com>
2018-10-09 23:27:27 +0900

    commit

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+import misaka 
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.library()
+
+class Group(models.Model):
+    name = models.CharField(max_length = 255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique = True)
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='memberships')
+    user = models.ForeignKey(User,related_name='user_groups')
+    description = models.TextField(blank = True,default = '')
+    description_html = model.TextField(editable = False,default = '',blank = True)
+    members = models.ManyToManyField(User,through = 'GroupMember')
+
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group','user')
+
+
